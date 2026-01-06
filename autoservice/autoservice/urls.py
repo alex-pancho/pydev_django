1b707996972ce00d77ea61102abf608f43885c9a
Oleksandr Panchenko <pan.pan.4it@gmail.com>
2026-01-06 19:54:02 +0200

    migrate and first app

--- autoservice/autoservice/urls.py
+++ autoservice/autoservice/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("lesson_1/", include("lesson_1.urls")),
+    path("", include("lesson_1.urls")),
 ]
